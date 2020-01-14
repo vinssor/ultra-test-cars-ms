9a8a8b65aa728833b618e88f3a5626081e6b2533
Vincent GILLES <vincent.gilles@gmail.com>
2020-01-15 00:17:58 +0100

    Improves API description

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
-// TODO Find how to import markdown file content
-const apiDescription = '\
-# Heading\n\
-\n\
-Text attributes _italic_, *italic*, __bold__, **bold**, \`monospace\`.\n\
-\n\
-Horizontal rule:\n\
-\n\
----\n\
-\n\
-Bullet list:\n\
-\n\
-  * apples\n\
-  * oranges\n\
-  * pears\n\
-  \n\
-Numbered list:\n\
-\n\
-  1. apples\n\
-  2. oranges\n\
-  3. pears\n\
-  \n\
-A [link](http://example.com).\n\
-\n\
-An image:\n\
-![Swagger logo](https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/favicon-32x32.png)\n\
-\n\
-Code block:\n\
-\n\
-\`\`\`\n\
-{\n\
-  "message": "Hello, world!"\n\
-}\n\
-\`\`\`\n\
-\n\
-Tables:\n\
-\n\
-| Column1 | Collumn2 |\n\
-| ------- | -------- |\n\
-| cell1   | cell2    |';
+import { ApiDescription } from './api.description';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
     .setTitle('Cars Microservice')
-    .setDescription(apiDescription)
+    .setDescription(ApiDescription)
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
---
+++ tpl/api.description.tpl
@@ -0,0 +1,1 @@
+export const ApiDescription: string = 'value';
