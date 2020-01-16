a3e73d5f2ff941f10b60bdc160899bde5e561428
Vincent GILLES <vincent.gilles@gmail.com>
2020-01-16 02:18:36 +0100

    Adds jobs service

--- src/car/car.entity.ts
+++ src/car/car.entity.ts
@@ -70,7 +70,7 @@ export class Car {
   @ApiHideProperty()
   @Exclude({ toPlainOnly: true })
   @Column({ default: false })
-  reduced?: boolean;
+  discountedPrice?: boolean;
 
   @ApiProperty()
   @IsDefined({ groups: [CREATE] })
